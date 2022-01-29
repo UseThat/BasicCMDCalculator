99ebb1a6badec3d3306111a6097d75a512f158c4
mateu <mateusz.wygoda>
2022-01-29 22:30:48 +0100

    Add method Calculator to Main program

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,3 +1,12 @@
 public class Calculate {
+    public static void main(String[] args) {
+        double a = Math.random();
+        double b = Math.random();
 
+        Calculator calc = new Calculator();
+        calc.add(a,b);
+        calc.subtract(a,b);
+        calc.divide(a,b);
+        calc.multiply(a,b);
+    }
 }
