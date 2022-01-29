a9e2b8870d90a9360f0d15590d71e4d5222ea124
mateu <mateusz.wygoda>
2022-01-29 22:22:31 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    void add(double a, double b){
+        System.out.println(a+b);
+    }
+
+    void subtract(double a, double b){
+        System.out.println(a-b);
+    }
+
+    void multiply(double a, double b){
+        System.out.println(a*b);
+    }
+
+    void divide(double a, double b){
+        if(b == 0){
+            System.out.println("Unable to divide by 0");
+        }else{
+            System.out.println(a/b);
+        }
+    }
+}
