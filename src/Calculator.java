046899f67527eb08cb4b29e97e3effa254a94cb2
mateu <mateusz.wygoda>
2022-01-29 22:25:52 +0100

    Add logic System out println

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,21 @@
 public class Calculator {
     void add(double a, double b){
-        System.out.println(a+b);
+        System.out.println(a+" + "+b+" = "+(a+b));
     }
 
     void subtract(double a, double b){
-        System.out.println(a-b);
+        System.out.println(a+" - "+b+" = "+(a-b));
     }
 
     void multiply(double a, double b){
-        System.out.println(a*b);
+        System.out.println(a+" * "+b+" = "+(a*b));
     }
 
     void divide(double a, double b){
         if(b == 0){
             System.out.println("Unable to divide by 0");
         }else{
-            System.out.println(a/b);
+            System.out.println(a+" / "+b+" = "+(a/b));
         }
     }
 }
